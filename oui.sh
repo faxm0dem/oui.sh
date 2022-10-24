f65b4763df9f89f23fdcd3abb45c30b9fe20d95a
Fabien Wernli <github@faxmodem.org>
2022-10-24 12:41:48 +0200

    Adapt to new ouich.es site

--- oui.sh
+++ oui.sh
@@ -1,18 +1,19 @@
-ouich ()
+ouich()
 {
-	if [ "$1" == "list" ]; then
-		lftp -e 'ls;exit' https://ouich.es/ | awk -F/ '/tag/{print $2}' | sort | column;
-	else
-		if [ -z "$1" ]; then
-			tag=$(curl -s https://ouich.es/random -I | awk -F': ' '$1 == "Location" {print $2}');
-			if [ -n "$tag" ]; then
-				echo $tag;
-				play mpv --really-quiet https://ouich.es$tag;
-			else
-				play mpv --really-quiet https://ouich.es/random;
-			fi;
+	arg1=$1
+	list=$(lftp -e 'ls;exit' https://ouich.es/ | awk -F'[/.]' '/tag/{print $2}' | shuf)
+	set -- $list
+	random=$1
+	if [ "$arg1" == "list" ]; then
+		echo $list | sort | column
+	elif [ -z "$arg1" ]; then
+		if [ -n "$random" ]; then
+			echo $random
+			play mpv --really-quiet https://ouich.es/mp3/${random}.mp3
 		else
-			play mpv --really-quiet https://ouich.es/mp3/$1.mp3;
-		fi;
+			play mpv --really-quiet https://ouich.es/random
+		fi
+	else
+		play mpv --really-quiet https://ouich.es/mp3/$1.mp3
 	fi
 }
